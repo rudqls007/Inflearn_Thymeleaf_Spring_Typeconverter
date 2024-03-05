a67dcb5c644bb95c39c4e9cedf9b2148bfc033e1
rudqls007 <rudqls007@gmail.com>
2024-03-06 01:45:11 +0900

    스프링 타입 컨버터 - 포맷터 적용하기

--- src/main/java/hello/typeconverter/Webconfig.java
+++ src/main/java/hello/typeconverter/Webconfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToipPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,9 +15,15 @@ public class Webconfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
+        //주석처리 우선순위
+        /*
         registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new IntegerToStringConverter());*/
         registry.addConverter(new StringToipPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
